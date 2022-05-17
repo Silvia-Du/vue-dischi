8039afd795b650c1646ec1e94907ad4fa60f5485
Silvia-Du <silviadu@hotmail.it>
2022-05-17 18:50:51 +0200

    Fine chiamata Axios & style

--- src/components/CardMain.vue
+++ src/components/CardMain.vue
@@ -1,21 +1,43 @@
 <template>
-  <div class="col-6 col-md-3 col-lg-2 debug">
-    <div class="card">
-      <img src="" alt="">
-      <h4>testo</h4>
-      <h4>testo</h4>
-      <h4>testo</h4>
-    </div>
-  </div>
 
+    <div class="_card text-center">
+      <img :src="cardItem.poster" :alt="cardItem.title">
+      <p>{{cardItem.title}}</p>
+      <p class="mb-0">{{cardItem.author}}</p>
+      <p>{{cardItem.year}}</p>
+    </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'CardMain'
+  name: 'CardMain',
+  props: {cardItem: Object}
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+  @import '../assets/style/vars.scss';
+
+  ._card{
+    background-color: $lightenColor;
+    padding: 15px;
+    margin: 0 15px 30px 15px;
+    width: calc((100% / 5) - 30px);
+
+    & p:first-of-type{
+      color: white;
+      font-size: 0.9rem;
+      text-transform: uppercase;
+    }
+    p{
+      color: #716f6f;
+    }
+    img{
+      width: 100%;
+      margin-bottom: 10px;
+    }
+  }
 
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,11 +1,12 @@
 <template>
 
-  <main>
-    <div class="container debug">
-      <div class="sd-row row debug d-flex justify-content-center">
+  <main class="d-flex align-items-center">
+    <div class="container">
+      <div class="sd-row row d-flex justify-content-center">
         
-        <CardMain />
-        
+        <CardMain 
+        v-for="(card, index) in cardsList" :key="`card-${index}`" 
+        :cardItem="card"/>
       </div>
     </div>
   </main>
@@ -13,17 +14,42 @@
 </template>
 
 <script>
+
 import CardMain from './CardMain.vue';
+import axios from 'axios';
+
 export default {
     name: "MainComp",
-    components: { CardMain }
+    components: { CardMain },
+
+    data(){
+      return{
+        apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+        cardsList :[],
+      }
+    },
+
+    
+    mounted() {
+      this.getDataDiscs();
+    },
+
+    methods:{
+      getDataDiscs(){
+        axios.get(this.apiUrl)
+        .then(response => {
+          this.cardsList = response.data.response;
+          
+        })
+      }
+    },
+
 }
 </script>
 
 <style lang="scss" scoped>
 
   main{
-    padding: 70px 0;
     height: calc(100vh - 80px);
     background-color: #1e2d3b;
     .sd-row{
